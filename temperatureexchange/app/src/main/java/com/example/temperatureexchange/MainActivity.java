8446d777adfcb7cfda0807dcfbecf20b054b3dbc
Pang-Yiyi <137187346+Pang-Yiyi@users.noreply.github.com>
2024-07-03 12:04:59 +0800

    部分完成溫度轉換計算機

--- temperatureexchange/app/src/main/java/com/example/temperatureexchange/MainActivity.java
+++ temperatureexchange/app/src/main/java/com/example/temperatureexchange/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.temperatureexchange;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,5 +24,32 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void C2F(View view){
+        TextView exchangeText = (TextView)findViewById(R.id.result);
+        Button C2FButton = (Button)findViewById(R.id.C2F);
+        EditText temText = (EditText)findViewById(R.id.temperature);
+
+        int temperature = Integer.parseInt(temText.getText().toString());
+        int result = Integer.parseInt(exchangeText.getText().toString());
+
+        result = temperature * 9/5 + 32;
+        exchangeText.setText(String.valueOf(result));
+
+
+    }
+
+    public void F2C(View view){
+        TextView exchangeText = (TextView)findViewById(R.id.result);
+        Button F2CButton = (Button)findViewById(R.id.F2C);
+        EditText temText = (EditText)findViewById(R.id.temperature);
+
+        int temperature = Integer.parseInt(temText.getText().toString());
+        int result = Integer.parseInt(exchangeText.getText().toString());
+
+        result = (temperature - 32) * 5/9;
+        exchangeText.setText(String.valueOf(result));
+
+
     }
 }
