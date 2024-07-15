78b03c505887269f9b0632e835a74b6b80548c33
Pang-Yiyi <137187346+Pang-Yiyi@users.noreply.github.com>
2024-07-15 12:01:34 +0800

    BMI功能製作（未完成）

--- BMI/app/src/main/java/com/example/bmi/MainActivity.java
+++ BMI/app/src/main/java/com/example/bmi/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.bmi;
 
+import android.app.Activity;
+import android.app.Instrumentation;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityResultLauncher<Intent> intentActivityResultLauncher;
+    private double bmi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,23 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        intentActivityResultLauncher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                new ActivityResultCallback<ActivityResult>() {
+                    @Override
+                    public void onActivityResult(ActivityResult result) {
+                        //寫另一個Activity回傳後，獲取回傳的資料之後的做法
+                        if(result.getData() != null && result.getResultCode() == Activity.RESULT_OK){
+                            bmi = result.getData().getDoubleExtra("BMI", -1);
+                        }
+                    }
+                }
+        );
+    }
+
+    public void Go2BMICalculate(View view){
+        Intent intent = new Intent(this, CalBMIActivity.class);
+
     }
 }
