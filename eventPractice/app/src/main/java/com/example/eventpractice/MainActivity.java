52617fc6e0bc9a76c86c910dfeb38e2eb5d772d9
Pang-Yiyi <137187346+Pang-Yiyi@users.noreply.github.com>
2024-07-31 11:20:35 +0800

    字體放大與縮小

--- eventPractice/app/src/main/java/com/example/eventpractice/MainActivity.java
+++ eventPractice/app/src/main/java/com/example/eventpractice/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.eventpractice;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,9 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View
+
+        .OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +25,26 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Button d = (Button) findViewById(R.id.increase);
+        Button i = (Button) findViewById(R.id.decrease);
+        d.setOnClickListener(this);
+        i.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        TextView tv_text = (TextView) findViewById(R.id.tv_text);
+        TextView tv_size = (TextView) findViewById(R.id.tv_size);
+
+        float currentSizeSp = tv_text.getTextSize() / getResources().getDisplayMetrics().scaledDensity;
+        if(view.getId() == R.id.increase){
+            tv_text.setTextSize(currentSizeSp + 5);
+            tv_size.setText(String.valueOf(currentSizeSp + 5));
+        }else if((view.getId() == R.id.decrease)){
+            tv_text.setTextSize(currentSizeSp - 5);
+            tv_size.setText(String.valueOf(currentSizeSp - 5));
+        }
+
     }
 }
